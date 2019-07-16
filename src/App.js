f99b4c5c0ee2829a2b7910972fc0108c0fbe14da
Laura <laurajadefogarty@gmail.com>
2019-07-16 16:25:37 +0100

    allows pathing to articles by topic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./Components/Footer.jsx";
 import Nav from "./Components/Nav.jsx";
 import Articles from "./Components/Articles.jsx";
 import Article from "./Components/ArticleCard.jsx";
+import PostArticle from "./Components/PostArticle";
 import "./CSS/App.css";
 import { Router } from "@reach/router";
 
@@ -19,9 +20,9 @@ class App extends Component {
         <Nav />
         <Router className="router">
           <Articles path="/" />
-          {/* <ArticlesByTopic path="articles/:topic" /> */}
+          <Articles path="/articles/:topic" />
           <Article path="articles/:topic/:article_id" />
-          {/* <PostNewArticle path="/newArticle" /> */}
+          <PostArticle path="/newArticle" />
         </Router>
         <Footer>Footer</Footer>
       </div>
