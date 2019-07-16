1ef906c77b20540f8e571c24e11860442219cf43
Laura <laurajadefogarty@gmail.com>
2019-07-16 16:27:26 +0100

    adds more information to article cards

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const ArticleCard = ({ article }) => {
   return (
     <div>
-      <div className="articleCard">
-        <div className="ArticleCard">
-          <h3 className="ArticleTitle">{article.title}</h3>
-          <p className="ArticleBody">{article.body}</p>
-          <p className="Author">{article.author}</p>
-        </div>
+      <div className="ArticleCard">
+        <h3 className="ArticleTitle">{article.title}</h3>
+        <p className="topic">r/{article.topic}</p>
+        <p className="ArticleBody">{article.body}</p>
+        <p className="Author">Posted by {article.author}</p>
+        <p className="Comments">Number of comments: {article.comment_count}</p>
+        <p className="Votes">Number of votes: {article.votes}</p>
+        <p className="Created At">Posted at {article.created_at}</p>
       </div>
     </div>
   );
