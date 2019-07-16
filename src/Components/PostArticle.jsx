7dd0a8eedefe139596eb1050d3bed0a8e4bc54d1
Laura <laurajadefogarty@gmail.com>
2019-07-16 16:33:34 +0100

    initial set up

---
+++ src/Components/PostArticle.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class PostArticle extends Component {
+  state = {
+    article: {}
+  };
+
+  render() {
+    const { article } = this.state;
+
+    return <div />;
+  }
+}
+
+export default PostArticle;
