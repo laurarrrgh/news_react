777684ba897041bee2791afcc4279e9cfb926a96
Laura <laurajadefogarty@gmail.com>
2019-07-16 12:42:25 +0100

    completes nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
+import * as api from "../utils/api.js";
+import "../CSS/Nav.css";
 
 class Nav extends Component {
+  state = {
+    topics: []
+  };
   render() {
+    const { topics } = this.state;
     return (
       <div>
-        <h1>Nav</h1>
+        <nav className="nav">
+          <Link to="/">All Articles</Link>
+          {topics.map(topic => (
+            <Link key={topic.slug} to={`/articles/${topic.slug}`}>
+              {topic.slug}
+            </Link>
+          ))}
+        </nav>
       </div>
     );
   }
+  componentDidMount() {
+    this.fetchTopics();
+  }
+  fetchTopics = () => {
+    api.getTopics().then(topics => {
+      this.setState({ topics });
+    });
+  };
 }
 
 export default Nav;
