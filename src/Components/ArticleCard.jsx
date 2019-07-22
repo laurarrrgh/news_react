c7a20d0410bde633ff540b838723e7c57eac67ee
Laura <laurajadefogarty@gmail.com>
2019-07-22 10:10:07 +0100

    simplifies date format

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "@reach/router";
+// import "../CSS/SingleArticle.css";
+import "../CSS/ArticleCard.css";
+import * as moment from "moment";
 
 const ArticleCard = ({ article }) => {
   return (
-    <div>
+    <div className="individualArticle">
       <div className="ArticleCard">
         <Link className="ArticleTitle" to={`/article/${article.article_id}`}>
           {article.title}
         </Link>
         <p className="topic">r/{article.topic}</p>
         <p className="ArticleBody">{article.body}</p>
-        <p className="Author">Posted by {article.author}</p>
-        <p className="Comments">Number of comments: {article.comment_count}</p>
-        <p className="Votes">Number of votes: {article.votes}</p>
-        <p className="Created At">Posted at {article.created_at}</p>
+        <Link className="comments" to={`/article/${article.article_id}`}>
+          Number of comments: {article.comment_count}
+        </Link>
+        <small className="small">
+          Posted by {article.author}
+          Number of votes: {article.votes}
+          Posted at:{moment(article.created_at).format("DD-MM-YYYY hh:mm:ss")}
+        </small>
       </div>
     </div>
   );
