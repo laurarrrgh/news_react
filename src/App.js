003c34c6334d02e182ff4b7b7dd33b168b646d4c
Laura <laurajadefogarty@gmail.com>
2019-07-22 16:42:18 +0100

    adds error page routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer.jsx";
 import Nav from "./Components/Nav.jsx";
 import Articles from "./Components/Articles.jsx";
-import Article from "./Components/ArticleCard.jsx";
+// import Article from "./Components/ArticleCard.jsx";
 import PostComment from "./Components/PostComment";
 import SingleArticle from "./Components/SingleArticle";
-import Comments from "./Components/SingleArticle";
+import Error from "./Components/Error";
+// import Comments from "./Components/SingleArticle";
 import "./CSS/App.css";
 import { Router } from "@reach/router";
 
@@ -16,13 +17,14 @@ class App extends Component {
       <div className="app">
         <Header />
         <Nav />
-        <Router className="router">
+        <Router className="router" primary={false}>
           <Articles path="/" />
           <Articles path="/:topic" />
           <PostComment path="/article/:article_id/post" />
           <SingleArticle path="/article/:article_id" />
+          <Error default />
         </Router>
-        <Footer>Footer</Footer>
+        <Footer />
       </div>
     );
   }
