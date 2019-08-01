e1f2841741703d5f6f7fbac93341b5b2adbca936
Laura <laurajadefogarty@gmail.com>
2019-08-01 21:07:26 +0100

    adds error handling

--- src/Components/DeleteComment.jsx
+++ src/Components/DeleteComment.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import * as api from "../utils/api.js";
+import { navigate } from "@reach/router";
 
 class DeleteComment extends Component {
   state = { author: "jessjelly", article_id: this.props.article_id };
@@ -16,7 +17,17 @@ class DeleteComment extends Component {
   handleSubmit = event => {
     event.preventDefault();
     const { comment_id } = this.props;
-    api.deleteComment(comment_id).then(this.props.deleteOwnComment(comment_id));
+    api
+      .deleteComment(comment_id)
+      .then(this.props.deleteOwnComment(comment_id))
+      .catch(err => {
+        navigate("/error", {
+          state: {
+            message: "Comment could not be deleted"
+          },
+          replace: true
+        });
+      });
   };
 }
 
