8721b9fc77758d4d4bd81be2250980618275ff54
Laura <laurajadefogarty@gmail.com>
2019-08-08 22:38:20 +0100

    adds voting styling

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -1,35 +1,47 @@
 import React, { Component } from "react";
 import * as api from "../utils/api.js";
 import "../CSS/SingleArticle.css";
+import "../CSS/Votes.css";
 import PostComment from "./PostComment.jsx";
 import DeleteComment from "./DeleteComment.jsx";
 import Votes from "./Votes.jsx";
 import * as moment from "moment";
 import { navigate } from "@reach/router";
+import Loading from "./Loading.jsx";
 
 class SingleArticle extends Component {
   state = {
     article: {},
-    comments: []
+    comments: [],
+    loading: true
   };
 
   render() {
-    const { article, comments } = this.state;
+    const { article, comments, loading } = this.state;
 
-    return (
+    return loading ? (
+      <Loading />
+    ) : (
       <div className="singleArticleMain">
-        <span className="leadarticle">
+        <span className="leadArticle">
           <h1 className="singleArticleHeader">{article.title}</h1>
-          <p className="articlebody">{article.body}</p>
-          <small className="smalldetails">
-            Posted by:{article.author} Posted at:
-            {moment(article.created_at).format("DD-MM-YYYY hh:mm")}
-          </small>
-          <Votes
-            votes={article.votes}
-            id={article.article_id}
-            section="articles"
-          />
+          <div className="gridBox">
+            <p className="articleBody">
+              {article.body}
+              <br />
+              <br />
+              <small className="smalldetails">
+                Posted by:{article.author} Posted at:
+                {moment(article.created_at).format("DD-MM-YYYY hh:mm")}
+              </small>
+            </p>
+            <Votes
+              className="voteBox"
+              votes={article.votes}
+              id={article.article_id}
+              section="articles"
+            />
+          </div>
         </span>
         <div className="comments">
           <PostComment
@@ -41,23 +53,30 @@ class SingleArticle extends Component {
           {comments.map(comment => {
             return (
               <li className="comment" key={comment.comment_id}>
-                <p>{comment.body}</p>
-                <small>Author: {comment.author} </small>
-                <small>
-                  Posted at:
-                  {moment(comment.created_at).format("DD-MM-YYYY hh:mm")}
-                </small>
-                {comment.author === "jessjelly" ? (
-                  <DeleteComment
-                    comment_id={comment.comment_id}
-                    deleteOwnComment={this.deleteOwnComment}
+                <div className="commentGridBox">
+                  <p className="commentBody">
+                    {comment.body}
+                    <br /> <br />
+                    <small>Author: {comment.author} </small>
+                    <small>
+                      Posted at:
+                      {moment(comment.created_at).format("DD-MM-YYYY hh:mm")}
+                    </small>
+                    {comment.author === "jessjelly" ? (
+                      <DeleteComment
+                        comment_id={comment.comment_id}
+                        deleteOwnComment={this.deleteOwnComment}
+                      />
+                    ) : null}
+                  </p>
+                  <br />
+                  <Votes
+                    className="voteBox"
+                    votes={comment.votes}
+                    id={comment.comment_id}
+                    section="comments"
                   />
-                ) : null}
-                <Votes
-                  votes={comment.votes}
-                  id={comment.comment_id}
-                  section="comments"
-                />
+                </div>
               </li>
             );
           })}
@@ -76,7 +95,7 @@ class SingleArticle extends Component {
     await api
       .getArticle(article_id)
       .then(article => {
-        this.setState({ article });
+        this.setState({ article, loading: false });
       })
 
       .catch(err => {
@@ -94,7 +113,7 @@ class SingleArticle extends Component {
     api
       .getComments(article_id)
       .then(comments => {
-        this.setState({ comments });
+        this.setState({ comments, loading: false });
       })
       .catch(err => {
         navigate("/error", {
