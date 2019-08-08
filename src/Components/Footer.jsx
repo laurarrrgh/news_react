5435d767e08204ff91dd754b4afd537a2267429b
Laura <laurajadefogarty@gmail.com>
2019-08-08 22:39:05 +0100

    adds footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "../CSS/Footer.css";
 
 const Footer = () => {
-  return <footer className="footer">Hire Me :)</footer>;
+  return (
+    <footer className="footer">
+      <a href="https://github.com/laurarrrgh/news_react">GitHub</a>{" "}
+      <a href="https://www.linkedin.com/in/laurafogarty/">LinkedIn</a>
+    </footer>
+  );
 };
 
 export default Footer;
